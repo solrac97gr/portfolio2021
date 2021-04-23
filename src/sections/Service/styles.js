13c013bc012bbb6f153565dba19d99de3f9d062c
Carlos <carlosgarcia_97gr@hotmail.com>
2021-04-22 20:23:28 -0500

    mobile version finished

--- src/sections/Service/styles.js
+++ src/sections/Service/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../../styles/devices";
 
 export const ServiceContainer = styled.section`
   display: flex;
@@ -6,20 +7,30 @@ export const ServiceContainer = styled.section`
   padding: 10% 6%;
   margin: 0;
   background-color: #393640;
-  justify-content:space-between;
+  justify-content: space-between;
+  @media ${device.mobileL} {
+    flex-direction: column-reverse;
+  }
 `;
 export const ServiceCardsColumn = styled.div`
   width: 60%;
   display: flex;
   flex-direction: column;
-  justify-content:space-between;
+  justify-content: space-between;
+  @media ${device.mobileL} {
+    width: 100%;
+  }
 `;
 export const ServiceIntroduceColumn = styled.div`
   width: 40%;
   display: flex;
   flex-direction: column;
   padding: 0% 11%;
-  margin:auto;
+  margin: auto;
+  @media ${device.mobileL} {
+    width: 100%;
+    padding: 0% 3%;
+  }
 `;
 export const Title = styled.h2`
   color: #95989f;
@@ -29,12 +40,18 @@ export const Title = styled.h2`
 export const PrincipalQuote = styled.span`
   color: white;
   font-size: 45px;
-  margin:34px 0px;
+  margin: 34px 0px;
+  @media ${device.mobileL} {
+    font-size: 34px;
+  }
 `;
 export const SecondaryQuote = styled.span`
   color: white;
   font-size: 21px;
   letter-spacing: 3px;
+  @media ${device.mobileL} {
+    font-size: 16px;
+  }
 `;
 export const Introduction = styled.p`
   color: #95989f;
